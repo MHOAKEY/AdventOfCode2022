13ecdb6cdb5c041b2170718c0b576fc8f5f225be
Matt Oake <matt@Matts-MacBook-Air.local>
2022-12-01 16:28:49 -0400

    looped through txt file to create a list of strings. looped list of str to convert each into int except blank str. looped list of intigers and kept track of the sum of each int between each blank str to make a new list. The new list will show each elfs total of cals. with the list of total cals, looped each index and kept the largest int. Also sorted total list and summed up last 3 to get the total of the top 3 total of calories.

--- day1.py
+++ day1.py
@@ -1,11 +1,39 @@
 arrayOfCals = []
 
 with open('elfCalories.txt') as f:
-    for line in f:
-        arrayOfCals.append(line.strip())
+    for eachLine in f:
+        arrayOfCals.append(eachLine.strip())
+        
 
-print(arrayOfCals)
+for index in range(0, len(arrayOfCals)):
+    if arrayOfCals[index] != "":
+        arrayOfCals[index] = int(arrayOfCals[index])
 
 
+eachElfCalTotalArray = []
+x = 0
+
+for num in arrayOfCals:
+    if num != "":
+        x += num
+    else: 
+        eachElfCalTotalArray.append(x)
+        x = 0
 
 
+mostCals = eachElfCalTotalArray[0]
+
+
+for i in range(0, len(eachElfCalTotalArray)):
+    if (eachElfCalTotalArray[i] > mostCals):
+        mostCals = eachElfCalTotalArray[i]
+
+
+eachElfCalTotalArray.sort()
+totalOfThree = 0
+
+for i in eachElfCalTotalArray[-3:]:
+    totalOfThree += i
+
+
+print(totalOfThree)
