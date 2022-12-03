2bd04156a4c6ef7f7fd0b59bc63310cb89ec058f
Matt Oake <matt@Matts-MacBook-Air.local>
2022-12-03 13:41:28 -0400

    playing with simple string to split it in half.

--- day3.py
+++ day3.py
@@ -4,21 +4,29 @@ with open("rucksacks.txt") as f:
     for each in f:
         rucksack.append(each.strip())
 
-one = "vJrwpWtwJgWr"
-two = "hcsFMMfFFhFp"
-list1 = []
-list2 = []
+# one = "vJrwpWtwJgWr"
+# two = "hcsFMMfFFhFp"
+# list1 = []
+# list2 = []
 
-match = []
+# match = []
 
-for char in one:
-    list1.append(char)
+# for char in one:
+#     list1.append(char)
 
-for char in two:
-    list2.append(char)
+# for char in two:
+#     list2.append(char)
 
-for element in list1:
-    if element in list2:
-        match.append(element)
+# for element in list1:
+#     if element in list2:
+#         match.append(element)
 
-print(match)
+# print(match)
+
+string = "vJrwpWtwJgWrhcsFMMfFFhFp"
+x = len(string)
+half1 = slice(0, x//2)
+half2 = slice(x//2, x)
+
+print(string[half1])
+print(string[half2])
