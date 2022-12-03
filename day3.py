07a2a2e768a512bfda447142587c97791d3d1c72
Matt Oake <matt@Matts-MacBook-Air.local>
2022-12-03 16:00:25 -0400

    removed comments.

--- day3.py
+++ day3.py
@@ -1,30 +1,3 @@
-# one = "vJrwpWtwJgWr"
-# two = "hcsFMMfFFhFp"
-# list1 = []
-# list2 = []
-
-# match = []
-
-# for char in one:
-#     list1.append(char)
-
-# for char in two:
-#     list2.append(char)
-
-# for element in list1:
-#     if element in list2:
-#         match.append(element)
-
-# print(match)
-
-# string = "vJrwpWtwJgWrhcsFMMfFFhFp"
-# x = len(string)
-# half1 = slice(0, x//2)
-# half2 = slice(x//2, x)
-
-# print(string[half1])
-# print(string[half2])
-
 rucksack = []
 splitRuckSack = []
 match = []
