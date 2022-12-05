17f9a94816ed4eea54b3a67a30ab36f9774e3e4c
Matt Oake <matt@Matts-MacBook-Air.local>
2022-12-05 15:40:31 -0400

    If there is or isnt an overlap in each comparison; append new to a new array true or false.

--- day4.py
+++ day4.py
@@ -16,3 +16,11 @@ for eachSection in array2:
     elf1End = eachSection[0][1]
     elf2Start = eachSection[1][0]
     elf2End = eachSection[1][1]
+    if (elf1Start > elf2Start and elf1End < elf2End) or (elf2Start > elf1Start and elf2End < elf1End):
+        eachSection = "true"
+        array3.append(eachSection)
+    else:
+        eachSection = "false"
+        array3.append(eachSection)
+
+print(array3)
