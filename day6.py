74c1d6e01cab159f2da840ef81c18a1142579288
Matt Oake <matt@Matts-MacBook-Air.local>
2022-12-06 14:20:06 -0400

    convert txt file into an array of strings.

--- day6.py
+++ day6.py
@@ -1,1 +1,8 @@
+dataStreamArray = []
+
 with open("day6.txt") as dataStream:
+    for data in dataStream:
+        for letter in data:
+            dataStreamArray.append(letter)
+
+print(dataStreamArray)
